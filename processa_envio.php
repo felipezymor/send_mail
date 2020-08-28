98919e2227fff8f3a41632bb96ea3e99f65a03b4
Luiz Felipe Fernandes <felipezymor@gmail.com>
2020-08-28 15:18:35 -0300

    Primeiro Commit

---
+++ processa_envio.php
@@ -0,0 +1,7 @@
+<?php
+require('../../private_send_mail/lib/PHPMailer/Exception.php');
+require('../../private_send_mail/lib/PHPMailer/OAuth.php');
+require('../../private_send_mail/lib/PHPMailer/PHPMailer.php');
+require('../../private_send_mail/lib/PHPMailer/POP3.php');
+require('../../private_send_mail/lib/PHPMailer/SMTP.php');
+require '../../private_send_mail/processa_envio.php';
